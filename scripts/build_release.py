64531be8b2e9bd8acd3e4204a744b286b384ca3c
Florian Bruhin <git@the-compiler.org>
2015-03-24 17:20:02 +0100

    fix print_title

--- scripts/build_release.py
+++ scripts/build_release.py
@@ -16,17 +16,17 @@ def call_script(name, *args, python=sys.executable):
 
 def build_common(args):
     """Common buildsteps used for all OS'."""
-    print_title("Running src2asciidoc.py")
+    utils.print_title("Running src2asciidoc.py")
     call_script('src2asciidoc.py')
 
-    print_title("Running asciidoc2html.py")
+    utils.print_title("Running asciidoc2html.py")
     if args.asciidoc is not None:
         a2h_args = ['--asciidoc'] + args.asciidoc
     else:
         a2h_args = []
     call_script('asciidoc2html.py', *a2h_args)
 
-    print_title("Running run_checks.py")
+    utils.print_title("Running run_checks.py")
     call_script('run_checks.py')
 
 
@@ -36,13 +36,13 @@ def build_windows():
     python_x86 = r'C:\Python{}_x32\python.exe'.format(ver)
     python_x64 = r'C:\Python{}\python.exe'.format(ver)
 
-    print_title("Running 32bit freeze.py build_exe")
+    utils.print_title("Running 32bit freeze.py build_exe")
     call_script('freeze.py', 'build_exe', python=python_x86)
-    print_title("Running 64bit freeze.py build_exe")
+    utils.print_title("Running 64bit freeze.py build_exe")
     call_script('freeze.py', 'build_exe', python=python_x64)
-    print_title("Running 32bit freeze.py bdist_msi")
+    utils.print_title("Running 32bit freeze.py bdist_msi")
     call_script('freeze.py', 'build_exe', python=python_x86)
-    print_title("Running 64bit freeze.py bdist_msi")
+    utils.print_title("Running 64bit freeze.py bdist_msi")
     call_script('freeze.py', 'build_exe', python=python_x64)
 
 
