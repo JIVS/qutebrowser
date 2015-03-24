38b58c28a3017f645814474e4bfe3109f7eee1c5
Florian Bruhin <git@the-compiler.org>
2015-03-24 17:18:17 +0100

    Add build_release

---
+++ scripts/build_release.py
@@ -0,0 +1,66 @@
+import os
+import sys
+import os.path
+import subprocess
+import argparse
+
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), os.pardir))
+
+from scripts import utils
+
+
+def call_script(name, *args, python=sys.executable):
+    path = os.path.join(os.path.dirname(__file__), name)
+    subprocess.check_call([python, path] + args)
+
+
+def build_common(args):
+    """Common buildsteps used for all OS'."""
+    print_title("Running src2asciidoc.py")
+    call_script('src2asciidoc.py')
+
+    print_title("Running asciidoc2html.py")
+    if args.asciidoc is not None:
+        a2h_args = ['--asciidoc'] + args.asciidoc
+    else:
+        a2h_args = []
+    call_script('asciidoc2html.py', *a2h_args)
+
+    print_title("Running run_checks.py")
+    call_script('run_checks.py')
+
+
+def build_windows():
+    """Build windows executables/setups."""
+    ver = ''.join([str(sys.version.major), str(sys.version.minor)])
+    python_x86 = r'C:\Python{}_x32\python.exe'.format(ver)
+    python_x64 = r'C:\Python{}\python.exe'.format(ver)
+
+    print_title("Running 32bit freeze.py build_exe")
+    call_script('freeze.py', 'build_exe', python=python_x86)
+    print_title("Running 64bit freeze.py build_exe")
+    call_script('freeze.py', 'build_exe', python=python_x64)
+    print_title("Running 32bit freeze.py bdist_msi")
+    call_script('freeze.py', 'build_exe', python=python_x86)
+    print_title("Running 64bit freeze.py bdist_msi")
+    call_script('freeze.py', 'build_exe', python=python_x64)
+
+
+def main():
+    """Main entry point."""
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--asciidoc', help="Full path to python and "
+                        "asciidoc.py. If not given, it's searched in PATH.",
+                        nargs=2, required=False,
+                        metavar=('PYTHON', 'ASCIIDOC'))
+    args = parser.parse_args()
+    utils.change_cwd()
+    if os.name == 'nt':
+        build_common(args)
+        build_windows()
+    else:
+        print("This script does nothing except on Windows currently.")
+
+
+if __name__ == '__main__':
+    main()
